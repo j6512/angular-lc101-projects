34e6c1472445421623f50253e7a5123f9dd1eea1
j6512 <justins8287@gmail.com>
2020-12-11 20:39:07 -0600

    finished

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   inactiveGold: boolean = false;
+   inactiveSilver: boolean = false;
+   inactiveCopper: boolean = false;
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
 
+   buttonToggle() {
+      this.inactiveGold = false;
+      this.inactiveSilver= false;
+      this.inactiveCopper = false;
+
+   }
+   
+   moveButton(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random() * 3)];
+      }
+
+      return this.location;
+   }
 }
--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
    skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-   alternateColor: string = 'black';
-   bulletType: string = 'A';
+   alternateColor: string = 'lightcoral';
+   bulletType: string = 'a';
    changeColor: boolean = true;
-
+   center: string = 'center';
+   lcLightBlue: string = 'lightblue';
    constructor() { }
 
    ngOnInit() { }
